e8c28bfb847fa76cbd428bd8826ff93659001b59
zy <584450903@qq.com>
2019-01-07 22:14:42 +0800

    Signed-off-by: zy <584450903@qq.com>

--- VideoPlayerProj/app/src/main/java/com/zy/videoproj/MainActivity.java
+++ VideoPlayerProj/app/src/main/java/com/zy/videoproj/MainActivity.java
@@ -1,13 +1,106 @@
 package com.zy.videoproj;
 
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
+import com.bumptech.glide.Glide;
+import com.zy.player.Player;
+import com.zy.player.PlayerAction;
+import com.zy.player.PlayerActionUser;
+
+/**
+ * Created by 周阳 on 19/1/7
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MyVideo myVideo = findViewById(R.id.myVideo);
+        myVideo.setUp("http://jzvd.nathen.cn/342a5f7ef6124a4a8faf00e738b8bee4/cf6d9db0bd4d41f59d09ea0a81e918fd-5287d2089db37e62345123a1be272f8b.mp4"
+                , "饺子快长大", MyVideo.SCREEN_WINDOW_NORMAL);
+        Glide.with(this).load("http://jzvd-pic.nathen.cn/jzvd-pic/1bb2ebbe-140d-4e2e-abd2-9e7e564f71ac.png")
+                .into(myVideo.thumbImageView);
+        Player.setJzUserAction(new MyUserActionStd());
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Player.releaseAllVideos();
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (Player.backPress()) {
+            return;
+        }
+        super.onBackPressed();
+    }
+
+    /**
+     * 这只是给埋点统计用户数据用的，不能写和播放相关的逻辑，
+     * 监听事件请参考MyUserActionStd，复写函数取得相应事件
+     */
+    class MyUserActionStd implements PlayerActionUser {
+
+        @Override
+        public void onEvent(int type, Object url, int screen, Object... objects) {
+            switch (type) {
+                case PlayerAction.ON_CLICK_START_ICON:
+                    Log.i("USER_EVENT", "ON_CLICK_START_ICON" + " title is : " + (objects.length == 0 ? "" : objects[0]) + " url is : " + url + " screen is : " + screen);
+                    break;
+                case PlayerAction.ON_CLICK_START_ERROR:
+                    Log.i("USER_EVENT", "ON_CLICK_START_ERROR" + " title is : " + (objects.length == 0 ? "" : objects[0]) + " url is : " + url + " screen is : " + screen);
+                    break;
+                case PlayerAction.ON_CLICK_START_AUTO_COMPLETE:
+                    Log.i("USER_EVENT", "ON_CLICK_START_AUTO_COMPLETE" + " title is : " + (objects.length == 0 ? "" : objects[0]) + " url is : " + url + " screen is : " + screen);
+                    break;
+                case PlayerAction.ON_CLICK_PAUSE:
+                    Log.i("USER_EVENT", "ON_CLICK_PAUSE" + " title is : " + (objects.length == 0 ? "" : objects[0]) + " url is : " + url + " screen is : " + screen);
+                    break;
+                case PlayerAction.ON_CLICK_RESUME:
+                    Log.i("USER_EVENT", "ON_CLICK_RESUME" + " title is : " + (objects.length == 0 ? "" : objects[0]) + " url is : " + url + " screen is : " + screen);
+                    break;
+                case PlayerAction.ON_SEEK_POSITION:
+                    Log.i("USER_EVENT", "ON_SEEK_POSITION" + " title is : " + (objects.length == 0 ? "" : objects[0]) + " url is : " + url + " screen is : " + screen);
+                    break;
+                case PlayerAction.ON_AUTO_COMPLETE:
+                    Log.i("USER_EVENT", "ON_AUTO_COMPLETE" + " title is : " + (objects.length == 0 ? "" : objects[0]) + " url is : " + url + " screen is : " + screen);
+                    break;
+                case PlayerAction.ON_ENTER_FULLSCREEN:
+                    Log.i("USER_EVENT", "ON_ENTER_FULLSCREEN" + " title is : " + (objects.length == 0 ? "" : objects[0]) + " url is : " + url + " screen is : " + screen);
+                    break;
+                case PlayerAction.ON_QUIT_FULLSCREEN:
+                    Log.i("USER_EVENT", "ON_QUIT_FULLSCREEN" + " title is : " + (objects.length == 0 ? "" : objects[0]) + " url is : " + url + " screen is : " + screen);
+                    break;
+                case PlayerAction.ON_ENTER_TINYSCREEN:
+                    Log.i("USER_EVENT", "ON_ENTER_TINYSCREEN" + " title is : " + (objects.length == 0 ? "" : objects[0]) + " url is : " + url + " screen is : " + screen);
+                    break;
+                case PlayerAction.ON_QUIT_TINYSCREEN:
+                    Log.i("USER_EVENT", "ON_QUIT_TINYSCREEN" + " title is : " + (objects.length == 0 ? "" : objects[0]) + " url is : " + url + " screen is : " + screen);
+                    break;
+                case PlayerAction.ON_TOUCH_SCREEN_SEEK_VOLUME:
+                    Log.i("USER_EVENT", "ON_TOUCH_SCREEN_SEEK_VOLUME" + " title is : " + (objects.length == 0 ? "" : objects[0]) + " url is : " + url + " screen is : " + screen);
+                    break;
+                case PlayerAction.ON_TOUCH_SCREEN_SEEK_POSITION:
+                    Log.i("USER_EVENT", "ON_TOUCH_SCREEN_SEEK_POSITION" + " title is : " + (objects.length == 0 ? "" : objects[0]) + " url is : " + url + " screen is : " + screen);
+                    break;
+
+                case PlayerActionUser.ON_CLICK_START_THUMB:
+                    Log.i("USER_EVENT", "ON_CLICK_START_THUMB" + " title is : " + (objects.length == 0 ? "" : objects[0]) + " url is : " + url + " screen is : " + screen);
+                    break;
+                case PlayerActionUser.ON_CLICK_BLANK:
+                    Log.i("USER_EVENT", "ON_CLICK_BLANK" + " title is : " + (objects.length == 0 ? "" : objects[0]) + " url is : " + url + " screen is : " + screen);
+                    break;
+                default:
+                    Log.i("USER_EVENT", "unknow");
+                    break;
+            }
+        }
     }
 }
